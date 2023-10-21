44e02d6bbec24e70aaa5c2da0d82b7b4b837fa22
unknown <jebaliseifeddine00@gmail.com>
2023-10-21 23:09:13 +0100

    feunf commit

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import './App.css';
 import UserList from './components/UserList';
 
 function App() {
-  return (
-    <div className="App">
-      <section className="blog-card">
-        <img
-          src="https://fastly.picsum.photos/id/949/1200/800.jpg?hmac=mW-_YmIqUMbyF5ydxz0QPn1GHneBWJEVlNCValTT5xw"
-          alt="white palace ceiling view"
-        />
-        <div className="blog-content">
-          <h1>Lorem ipsum dolor sit amet.</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-            aliquid sunt temporibus repellendus neque, quibusdam exercitationem.
-          </p>
-          <div className="author">
-            <img
-              src="https://fastly.picsum.photos/id/821/600/600.jpg?hmac=dW9EOnYxt9yoVvUndFExUXzPNEYbMjpYcKj3Au5zEWQ"
-              alt="author portrait"
-            />
-          </div>
-        </div>
-      </section>
+return (
+<div className="App">
+<section className="blog-card">
+<img
+src="https://fastly.picsum.photos/id/949/1200/800.jpg?hmac=mW-_YmIqUMbyF5ydxz0QPn1GHneBWJEVlNCValTT5xw"
+alt="white palace ceiling view"
+/>
+<div className="blog-content">
+<h1>Lorem ipsum dolor sit amet.</h1>
+<p>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+aliquid sunt temporibus repellendus neque, quibusdam exercitationem.
+</p>
+<div className="author">
+<img
+src="https://fastly.picsum.photos/id/821/600/600.jpg?hmac=dW9EOnYxt9yoVvUndFExUXzPNEYbMjpYcKj3Au5zEWQ"
+alt="author portrait"
+/>
+</div>
+</div>
+</section>
 
-      <UserList />
-    </div>
-  );
+<UserList />
+</div>
+);
 }
 
 export default App;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -4,34 +4,35 @@ import axios from 'axios';
 function UserList() {
   const [listOfUsers, setListOfUsers] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-        setListOfUsers(response.data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-      }
-    };
+useEffect(() => {
+const fetchData = async () => {
+try {
+const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+setListOfUsers(response.data);
+} catch (error) {
+console.error('Error fetching data: ', error);
+}
+};
 
-    fetchData();
-  }, []);
+fetchData();
+}, []);
 
-  return (
-    <div className="user-card-list">
-      {listOfUsers.map((user) => (
-        <div key={user.id} className="user-card">
-          <h2>{user.name}</h2>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Adresse: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
+
+return (
+<div className="user-card-list">
+{listOfUsers.map((user) => (
+<div key={user.id} className="user-card">
+<h2>{user.name}</h2>
+<p>Username: {user.username}</p>
+<p>Email: {user.email}</p>
+<p>Adresse: {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}</p>
 <p>Téléphone: {user.phone}</p>
 <p>Site web: {user.website}</p>
 <p>Entreprise: {user.company.name}</p>
-        </div>
-      ))}
-    </div>
-  );
+</div>
+))}
+</div>
+);
 }
 
 export default UserList;
